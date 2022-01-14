73e46890a7b4570b8b119d39003299d2a92f4b59
Kangyi LI <kangyi.li@polynom.io>
2022-01-14 01:41:28 +0100

    add config file

--- lokilogger/logging.py
+++ lokilogger/logging.py
@@ -1,80 +1,27 @@
-import colorlog
 import logging
-DEFAULT_FORMATTER_COLOR = '%(log_color)s"severity": "%(levelname)s", "logger": "%(name)s","module": "%(module)s", "message": "%(message)s"'
-DEFAULT_FORMATTER = '"severity": "%(levelname)s", "logger": "%(name)s","module": "%(module)s", "message": "%(message)s"'
+import pkg_resources
 
-
-class LokiLogging:
-    """This a custom `logging` for `Loki`.
-    """
-
-    def __init__(self):
-        """Init LokiLogging which will initialize an empty dictioary of loggers.
-        """
-        self.loggers = dict()
-
-    def getLogger(self, name: str) -> logging.Logger:
-        """Get a logger by its name.
-
-        Args:
-            name (str): A logger's name.
-
-        Returns:
-            logging.Logger: Return `logging.Logger` object.
-        """
-        try:
-            return self.loggers[name]
-        except KeyError:
-            raise Exception(f'Logger {name} doesn\'t exist')
-
-    def setLogger(self, name: str, custom_formatter=DEFAULT_FORMATTER_COLOR) -> logging.Logger:
-        """Set a logger by giving a name.
-
-        Args:
-            name (str): A new logger's name.
-            custom_formatter (str, optional): Setup a custom formatter which will overwrite the default one. Defaults to DEFAULT_FORMATTER_COLOR.
-
-        Returns:
-            logging.Logger: Return `logging.Logger` object.
-        """
-        logger = colorlog.getLogger(name)
-        handler = logging.StreamHandler()
-        handler.setFormatter(colorlog.ColoredFormatter(
-            custom_formatter))
-        logger.addHandler(handler)
-        self.loggers[name] = logger
-        return logger
-
-    def disableColor(self, name: str, custom_formatter=DEFAULT_FORMATTER) -> logging.Logger:
-        """Disable Colors.
-
-        Args:
-            name (str): The logger's name.
-            custom_formatter (str, optional): Setup a custom formatter which will overwrite the default one. Defaults to DEFAULT_FORMATTER.
-
-        Returns:
-            logging.Logger: Return `logging.Logger` object.
-        """
-        handler = logging.StreamHandler()
-        handler.setFormatter(logging.Formatter(custom_formatter))
-        logger = self.getLogger(name)
-        logger.removeHandler(logger.handlers[0])
-        logger.addHandler(handler)
-        return logger
-
-    def enableColor(self, name: str, custom_formatter=DEFAULT_FORMATTER_COLOR) -> logging.Logger:
-        """Enable Colors.
-
-        Args:
-            name (str): The logger's name.
-            custom_formatter (str, optional): Setup a custom formatter which will overwrite the default one. Defaults to DEFAULT_FORMATTER_COLOR.
-
-        Returns:
-            logging.Logger: Return `logging.Logger` object.
-        """
-        handler = logging.StreamHandler()
-        handler.setFormatter(colorlog.ColoredFormatter(custom_formatter))
-        logger = self.getLogger(name)
-        logger.removeHandler(logger.handlers[0])
-        logger.addHandler(handler)
-        return logger
+def setLogMode(mode:str):
+    path = "config.ini"
+    filepath = pkg_resources.resource_filename(__name__, path)
+    logging.config.fileConfig(filepath)
+    logger = logging.getLogger()
+    prod = logger.handlers[0]
+    dev = logger.handlers[1]
+    color = logger.handlers[2]
+    mode = mode.upper()
+    if mode == "PROD":
+        logger.removeHandler(dev)
+        logger.removeHandler(color)
+        logger.info(f"Dev env is set to production")
+    elif mode == "DEV_NO_COLOR":
+        logger.removeHandler(color)
+        logger.removeHandler(prod)
+        logger.info(f"Dev env is set to development without colorlog")
+    else:
+        logger.removeHandler(prod)
+        logger.removeHandler(dev)
+        if mode!= "DEV":
+            logger.warning(f"Unrecognized mode, default to development")
+        logger.info(f"Dev env is set to development")
+        
