50e2a24f0c6fe58724bcee1c4b93f0be2ff2c536
Kangyi LI <kangyi.li@polynom.io>
2022-01-14 01:41:28 +0100

    use config file for logger

--- example/example.py
+++ example/example.py
@@ -1,20 +1,6 @@
-from lokilogger.logging import LokiLogging
-logging = LokiLogging()
-logger1 = logging.setLogger("testlogger1")
+from lokilogger.logging import set_log_mode
+import logging
 
-DEFAULT_FORMATTER = '"severity": "%(levelname)s", , "message": "%(message)s"'
-
-# Set logger with custom formatter
-logger2 = logging.setLogger("testlogger2", DEFAULT_FORMATTER)
-
-logger1.info("This is an debug message")
-
-logging.disableColor("testlogger1")
-
-logger1.warning("This is an warning message")
-
-logging.enableColor("testlogger1")
-
-logger1.warning("This is an warning message")
-
-logger2.warning("This is an warning message")
+set_log_mode("DEV")
+logger = logging.getLogger(__name__)
+logger.error('error message')
